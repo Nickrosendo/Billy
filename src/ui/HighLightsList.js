63c7d3d6a1d6373b8c99e34b775075d154fb1cab
Nicolas Rosendo <nicolasrosendo@hotmail.com>
2018-08-01 22:30:25 -0300

    highlightList

---
+++ src/ui/HighLightsList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { StyleSheet, FlatList } from 'react-native'
+
+export default class HighLightsList extends Component {
+    render() {
+        return (
+            <FlatList
+            horizontal={true}/>
+        )
+    }
+}
---
+++ src/ui/HighLightsListItem.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { StyleSheet, View } from 'react-native'
+
+export default HighLightListItem = () => {
+    return (
+        <View style={styles.container}>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 0.2,
+        background: '#eee'
+    }
+})
